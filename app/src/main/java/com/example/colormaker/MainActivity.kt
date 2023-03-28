028dcea3cfdb6ecded38db7e701137f7f23969dc
Manchikalapudi <srisainavya@csu.fullerton.edu>
2023-03-28 16:13:01 -0700

    Persistence

--- app/src/main/java/com/example/colormaker/MainActivity.kt
+++ app/src/main/java/com/example/colormaker/MainActivity.kt
@@ -1,37 +1,62 @@
 package com.example.colormaker
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private var boxColor = intArrayOf(255, 0, 0)
+//    private var boxColor = intArrayOf(255, 0, 0)
     private lateinit var ResultColor: TextView
+    private lateinit var seekbar:SeekBar
+    private lateinit var seekbar1:SeekBar
+    private lateinit var seekbar2:SeekBar
+    private lateinit var switch:Switch
+    private lateinit var switch1:Switch
+    private lateinit var switch2:Switch
+    private lateinit var textView:EditText
+    private lateinit var textView1:EditText
+    private lateinit var textView2:EditText
     private val seekBarIds = arrayOf(R.id.seekBar, R.id.seekBar2, R.id.seekBar3)
     private val textViewIds = arrayOf(R.id.textView, R.id.textView2, R.id.textView3)
     private val switchIds = arrayOf(R.id.switch1, R.id.switch2, R.id.switch3)
-    private val memColor = intArrayOf(0, 0, 0)
+    val colormap= arrayOf("red","green","blue")
+    private lateinit var Vm:MyViewModel
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ResultColor = findViewById<TextView>(R.id.Rclr)
-
+        ConnectViews()
+        MyPreferencesRepository.initialize(this)
+        Vm=ViewModelProvider(this)[MyViewModel::class.java]
+        lifecycleScope.launch{
+            loadUIFlows()
+        }
+        Vm.flowUIValues()
         seekBarIds.forEachIndexed { index, sbId ->
             val mySeekBar = findViewById<SeekBar>(sbId)
-            mySeekBar.isEnabled = false
             mySeekBar.setOnSeekBarChangeListener(createSeekBarListener(index))
         }
 
         switchIds.forEachIndexed { index, switchId ->
             val switchButton = findViewById<Switch>(switchId)
-            switchButton.isChecked = false
             switchButton.setOnCheckedChangeListener(createSwitchListener(index))
         }
         for (i in 0..2){
@@ -40,24 +65,18 @@ class MainActivity : AppCompatActivity() {
             edt.addTextChangedListener(object : TextWatcher {
                 override fun afterTextChanged(s: Editable) {
 
-                    var rs=resources.getIdentifier(seekBarIds[i].toString(), "id", packageName)
-                    var isb=findViewById<View>(rs) as SeekBar
                     try {
                         var stt=s.toString().toFloat()
                         if (stt>1){
                             stt=1F
-
                             val message = "Max Value is 1!"
                             val duration = Toast.LENGTH_SHORT // or Toast.LENGTH_LONG
                             val toast = Toast.makeText(applicationContext, message, duration)
                             edt.setText("1.00")
                             toast.show()
-
                         }
                         var rbgvalue=(stt*255).toInt()
-                        boxColor[i]=rbgvalue
-                        isb.setProgress(rbgvalue)
-                        ResultColor?.setBackgroundColor(Color.argb(255, boxColor[0], boxColor[1], boxColor[2]))
+                        Vm.saveColor(rbgvalue,colormap[i])
 
                     }
                     catch (e:Exception){
@@ -77,51 +96,121 @@ class MainActivity : AppCompatActivity() {
 
         }
         val resetButton = findViewById<Button>(R.id.button)
-        resetButton.setOnClickListener { reset() }
-    }
+        resetButton.setOnClickListener {
+            Vm.saveColor(0,"red")
+            Vm.saveColor(0,"green")
+            Vm.saveColor(0,"blue")
+            Vm.saveState(false,"red")
+            Vm.saveState(false,"green")
+            Vm.saveState(false,"blue")
 
+        }
+
+
+
+    }
+    fun ConnectViews(){
+        seekbar=findViewById(R.id.seekBar)
+        seekbar1=findViewById(R.id.seekBar2)
+        seekbar2=findViewById(R.id.seekBar3)
+        switch=findViewById(R.id.switch1)
+        switch1=findViewById(R.id.switch2)
+        switch2=findViewById(R.id.switch3)
+        textView=findViewById(R.id.textView)
+        textView1=findViewById(R.id.textView2)
+        textView2=findViewById(R.id.textView3)
+
+    }
+    private suspend fun loadUIFlows(){
+        lifecycleScope.async {
+            Vm._gcl.asStateFlow().collectLatest {
+                seekbar1.progress = it
+                textView1.setText(String.format("%.2f", (it.toDouble() / 255.toDouble())))
+            }
+        }
+        lifecycleScope.async {
+            Vm._rcl.asStateFlow().collectLatest {
+                seekbar.progress = it
+                textView.setText(String.format("%.2f", (it.toDouble() / 255.toDouble())))
+            }
+        }
+        lifecycleScope.async {
+            Vm._bcl.asStateFlow().collectLatest {
+                seekbar2.progress = it
+                textView2.setText(String.format("%.2f", (it.toDouble() / 255.toDouble())))
+            }
+        }
+
+        lifecycleScope.async {
+            combine(Vm._allcl.asStateFlow(),Vm._allst.asStateFlow()){
+                    s,c->
+                Pair(s,c)
+            }.collectLatest {
+                var st=it.second
+                var colors=it.first
+                var mem=IntArray(3)
+                var rc=if (st[0]) colors[0] else 0
+                var gc=if (st[1]) colors[1] else 0
+                var bc=if (st[2]) colors[2] else 0
+                findViewById<TextView>(R.id.Rclr).setBackgroundColor(Color.argb(255,rc,gc,bc))
+
+            }
+        }
+        lifecycleScope.async {
+            Vm._rsw.asStateFlow().collectLatest {
+                switch.isChecked=it
+                textView.isEnabled =it
+                seekbar.isEnabled = it
+            }
+
+        }
+
+        lifecycleScope.async {
+            Vm._gsw.asStateFlow().collectLatest {
+                switch1.isChecked=it
+                textView1.isEnabled =it
+                seekbar1.isEnabled = it
+            }
+
+        }
+
+        lifecycleScope.async {
+            Vm._bsw.asStateFlow().collectLatest {
+                switch2.isChecked=it
+                textView2.isEnabled =it
+                seekbar2.isEnabled = it
+            }
+
+        }
+
+    }
+    
     private fun createSeekBarListener(index: Int): SeekBar.OnSeekBarChangeListener {
         return object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                boxColor[index] = progress
-                memColor[index] = boxColor[index]
-                val myTv = findViewById<TextView>(textViewIds[index])
-                myTv.text = String.format("%.2f", progress.toDouble() / 255.0)
-                ResultColor.setBackgroundColor(Color.argb(255, boxColor[0], boxColor[1], boxColor[2]))
+
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                Vm.saveColor(seekBar.progress,colormap[index])
+            }
         }
     }
+
+
+
+
 
     private fun createSwitchListener(index: Int): CompoundButton.OnCheckedChangeListener {
         return CompoundButton.OnCheckedChangeListener { _, isChecked ->
             val sb = findViewById<SeekBar>(seekBarIds[index])
+            Vm.saveState(isChecked,colormap[index])
 
-            if (!isChecked) {
-                sb.isEnabled=false
-                memColor[index] = boxColor[index]
-                boxColor[index] = 0
-            } else {
-                sb.isEnabled=true
-                boxColor[index] = memColor[index]
-            }
-            ResultColor.setBackgroundColor(Color.argb(255, boxColor[0], boxColor[1], boxColor[2]))
-        }.also { switchListener ->
-            val switchButton = findViewById<Switch>(switchIds[index])
-            switchButton.isChecked = false
-            switchButton.setOnCheckedChangeListener { buttonView, isChecked ->
-                switchListener.onCheckedChanged(buttonView, isChecked)
-                findViewById<SeekBar>(seekBarIds[index]).isEnabled = isChecked
             }
         }
+
+
     }
-    private fun reset() {
-        boxColor = intArrayOf(0, 0, 0)
-        ResultColor.setBackgroundColor(Color.argb(255, boxColor[0], boxColor[1], boxColor[2]))
-        seekBarIds.forEach { findViewById<SeekBar>(it).progress = 0 }
-        textViewIds.forEach { findViewById<TextView>(it).text = "0" }
-    }
-}
+
 
